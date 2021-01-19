8b7e8825cca0f8c6138f37c071a24ee34974f763
Igor <igor.custodio8@gmail.com>
2021-01-19 18:30:10 -0300

    feat: add AddAcount usecase

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Igor Custodio on 19/01/21.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
---
+++ Domain/AddAcount.swift
@@ -0,0 +1,26 @@
+//
+//  AddAcount.swift
+//  Domain
+//
+//  Created by Igor Custodio on 19/01/21.
+//
+
+import Foundation
+
+protocol AddAcount {
+    func add(addAcountModel: AddAcountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+}
+
+struct AddAcountModel {
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
