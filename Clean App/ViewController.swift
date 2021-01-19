55a0065eb3036877690dc0799aa33875d60171d1
Igor <igor.custodio8@gmail.com>
2021-01-19 18:16:45 -0300

    Initial Commit

---
+++ Clean App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean App
+//
+//  Created by Igor Custodio on 19/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
