9d75080d083ee6c81a30fc606a75e15ac4813623
Igor <igor.custodio8@gmail.com>
2021-01-20 11:43:14 -0300

    refactor: move sut creation to a factory creation method

--- DataTests/RemoteAddAcountTests.swift
+++ DataTests/RemoteAddAcountTests.swift
@@ -31,20 +31,14 @@ class RemoteAddAcountTests: XCTestCase {
 
     func test_add_should_call_http_client_with_correct_url() throws {
         let url = URL(string: "http://any-url.com")!
-        let httpClientSpy = HttpClientSpy()
-        
-        let sut = RemoteAddAcount(url: url, httpClient: httpClientSpy)
+        let (sut, httpClientSpy) = makeSut(url: url)
         
         sut.add(addAcountModel: makeAddAccountModel())
-        
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
     func test_add_should_call_http_client_with_correct_data() throws {
-        
-        let httpClientSpy = HttpClientSpy()
-        
-        let sut = RemoteAddAcount(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy)
+        let (sut, httpClientSpy) = makeSut()
         let addAcountModel = makeAddAccountModel()
         sut.add(addAcountModel: makeAddAccountModel())
         
@@ -58,6 +52,12 @@ class RemoteAddAcountTests: XCTestCase {
 extension RemoteAddAcountTests {
     func makeAddAccountModel() -> AddAcountModel {
         return AddAcountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
+    }
+    
+    func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteAddAcount, httpClientSpy: HttpClientSpy) {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAcount(url: url, httpClient: httpClientSpy)
+        return (sut, httpClientSpy)
     }
     
     class HttpClientSpy: HttpPostClient {
