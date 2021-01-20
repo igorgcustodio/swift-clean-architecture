0b98ce2cb86036b6721166a6617a051cbc3f0b27
Igor <igor.custodio8@gmail.com>
2021-01-20 11:33:40 -0300

    refactor: move add acount model creation to a factory helper method

--- DataTests/RemoteAddAcountTests.swift
+++ DataTests/RemoteAddAcountTests.swift
@@ -34,8 +34,8 @@ class RemoteAddAcountTests: XCTestCase {
         let httpClientSpy = HttpClientSpy()
         
         let sut = RemoteAddAcount(url: url, httpClient: httpClientSpy)
-        let addAcountModel = AddAcountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
-        sut.add(addAcountModel: addAcountModel)
+        
+        sut.add(addAcountModel: makeAddAccountModel())
         
         XCTAssertEqual(httpClientSpy.url, url)
     }
@@ -45,8 +45,8 @@ class RemoteAddAcountTests: XCTestCase {
         let httpClientSpy = HttpClientSpy()
         
         let sut = RemoteAddAcount(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy)
-        let addAcountModel = AddAcountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
-        sut.add(addAcountModel: addAcountModel)
+        let addAcountModel = makeAddAccountModel()
+        sut.add(addAcountModel: makeAddAccountModel())
         
         let data = try? JSONEncoder().encode(addAcountModel)
         
@@ -56,6 +56,10 @@ class RemoteAddAcountTests: XCTestCase {
 
 // MARK: Helpers
 extension RemoteAddAcountTests {
+    func makeAddAccountModel() -> AddAcountModel {
+        return AddAcountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
+    }
+    
     class HttpClientSpy: HttpPostClient {
         var url: URL?
         var data: Data?
