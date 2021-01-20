080fa756c9b2c11747c5dfe96198def9fc60094d
Igor <igor.custodio8@gmail.com>
2021-01-20 17:38:07 -0300

    refactor: ensure remote add account calls http client only once

--- DataTests/UseCases/RemoteAddAcountTests.swift
+++ DataTests/UseCases/RemoteAddAcountTests.swift
@@ -16,7 +16,7 @@ class RemoteAddAcountTests: XCTestCase {
         let (sut, httpClientSpy) = makeSut(url: url)
         
         sut.add(addAcountModel: makeAddAccountModel())
-        XCTAssertEqual(httpClientSpy.url, url)
+        XCTAssertEqual(httpClientSpy.urls, [url])
     }
     
     func test_add_should_call_http_client_with_correct_data() throws {
@@ -41,11 +41,11 @@ extension RemoteAddAcountTests {
     }
     
     class HttpClientSpy: HttpPostClient {
-        var url: URL?
+        var urls = [URL]()
         var data: Data?
         
         func post(to url: URL, with data: Data?) {
-            self.url = url
+            self.urls.append(url)
             self.data = data
         }
     }
