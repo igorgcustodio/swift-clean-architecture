d84ec4f776568c14ff77078e5de159bfa610b38c
Igor <igor.custodio8@gmail.com>
2021-01-21 15:53:25 -0300

    refactor: add data extension to convert to json

--- Data/Helpers/ExtensionHelpers.swift
+++ Data/Helpers/ExtensionHelpers.swift
@@ -11,4 +11,8 @@ public extension Data {
     func toModel<T: Decodable>() -> T? {
         return try? JSONDecoder().decode(T.self, from: self)
     }
+    
+    func toJson() -> [String: Any]? {
+        return try? JSONSerialization.jsonObject(with: self, options: .allowFragments) as? [String: Any]
+    }
 }
--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import Alamofire
+import Data
 
 class AlamofireAdapter {
     private let session: Session
@@ -16,8 +17,7 @@ class AlamofireAdapter {
     }
     
     func post(to url: URL, with data: Data?) {
-        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
-        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
+        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).resume()
     }
 }
 
