3243496628e38aada8a66dd013cd1d0650cf5681
Igor <igor.custodio8@gmail.com>
2021-01-21 17:55:11 -0300

    fix: changing completion type on HttpClientSpy

--- DataTests/Http/HttpClientSpy.swift
+++ DataTests/Http/HttpClientSpy.swift
@@ -11,7 +11,7 @@ import Data
 class HttpClientSpy: HttpPostClient {
     var urls = [URL]()
     var data: Data?
-    var completion: ((Result<Data, HttpError>) -> Void)?
+    var completion: ((Result<Data?, HttpError>) -> Void)?
     
     func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
         self.urls.append(url)
