04a1643d5dc45e75080713bfee3286a253bef246
Igor <igor.custodio8@gmail.com>
2021-01-21 15:18:57 -0300

    test: ensure AlamofireAdapter can calls Alamofire without data

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -16,14 +16,14 @@ class AlamofireAdapter {
     }
     
     func post(to url: URL, with data: Data?) {
-        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
         session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
 
 class AlamofireAdapterTests: XCTestCase {
 
-    func test_post_should_make_request_with_valid_urland_method() throws {
+    func test_post_should_make_request_with_valid_url_and_method() throws {
         let url = makeUrl()
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
@@ -32,10 +32,26 @@ class AlamofireAdapterTests: XCTestCase {
         sut.post(to: url, with: makeValidData())
         
         let exp = expectation(description: "waiting")
-        UrlProtocolStub.observerRequest { (request) in
+        UrlProtocolStub.observerRequest { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
             XCTAssertNotNil(request.httpBodyStream)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_post_should_make_request_with_no_data() throws {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        sut.post(to: url, with: nil)
+        
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observerRequest { request in
+            XCTAssertNil(request.httpBodyStream)
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
