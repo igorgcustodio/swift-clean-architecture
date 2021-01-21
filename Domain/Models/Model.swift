20c9bec12a20e9aea102fc8bb6e594e469d7fd1b
Igor <igor.custodio8@gmail.com>
2021-01-21 14:16:04 -0300

    feat: add infra layer

--- Domain/Models/Model.swift
+++ Domain/Models/Model.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 public protocol Model: Codable, Equatable {
-    
 }
 
 
---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Igor Custodio on 21/01/21.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_() throws {
+    }
+
+}
