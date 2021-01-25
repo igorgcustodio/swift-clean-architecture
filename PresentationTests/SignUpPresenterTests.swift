49452eacd00487f81eac0e6fa729366fd4de3eea
Igor <igor.custodio8@gmail.com>
2021-01-25 20:47:20 -0300

    tests: ensure SignUpPresenter shows error if password confirmation isn't provided

--- PresentationTests/SignUpPresenterTests.swift
+++ PresentationTests/SignUpPresenterTests.swift
@@ -22,6 +22,8 @@ class SignUpPresenter {
             self.alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Email é obrigatório"))
         } else if viewModel.password == nil || viewModel.password!.isEmpty {
             self.alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Senha é obrigatória"))
+        } else if viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty {
+            self.alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Confirmar senha é obrigatório"))
         }
     }
 }
@@ -63,6 +65,13 @@ class SignUpPresenterTests: XCTestCase {
         let signUpViewModel = SignUpViewModel(name: "any_name", email: "any_email@mail.com", passwordConfirmation: "any_password")
         sut.signUp(viewModel: signUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Senha é obrigatória"))
+    }
+    
+    func test_signup_should_show_error_message_if_password_confirmation_is_not_provided() {
+        let (sut, alertViewSpy) = makeSut()
+        let signUpViewModel = SignUpViewModel(name: "any_name", email: "any_email@mail.com", password: "any_password")
+        sut.signUp(viewModel: signUpViewModel)
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Confirmar senha é obrigatório"))
     }
 }
 
