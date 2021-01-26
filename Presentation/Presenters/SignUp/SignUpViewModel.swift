74d68d72848c5c0f831f6f577264c9d989285158
Igor <igor.custodio8@gmail.com>
2021-01-26 18:56:31 -0300

    test: ensure SignUpViewController calls signUp with correct values

--- Presentation/Presenters/SignUp/SignUpViewModel.swift
+++ Presentation/Presenters/SignUp/SignUpViewModel.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import Domain
 
-public struct SignUpViewModel {
+public struct SignUpViewModel: Model {
     public var name: String?
     public var email: String?
     public var password: String?
--- UI/SignUpViewController.swift
+++ UI/SignUpViewController.swift
@@ -12,6 +12,10 @@ final class SignUpViewController: UIViewController {
     
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
     
     var signUp: ((SignUpViewModel) -> Void)?
     
@@ -25,7 +29,8 @@ final class SignUpViewController: UIViewController {
     }
     
     @objc private func saveButtonTapper() {
-        signUp?(SignUpViewModel(name: nil, email: nil, password: nil, passwordConfirmation: nil))
+        let viewModel = SignUpViewModel(name: nameTextField.text, email: emailTextField.text, password: passwordTextField.text, passwordConfirmation: passwordConfirmationTextField.text)
+        signUp?(viewModel)
     }
 }
 
