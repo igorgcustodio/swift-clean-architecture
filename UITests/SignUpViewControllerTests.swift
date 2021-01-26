70b2c11aaa192cb04d3c5a82df27d76a6444dfc4
Igor <igor.custodio8@gmail.com>
2021-01-26 17:54:41 -0300

    feat: add UI layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Igor Custodio on 26/01/21.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
