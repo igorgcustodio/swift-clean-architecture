e8bd39cfe4a7bc285c99adae31903b8ee5909b00
Igor <igor.custodio8@gmail.com>
2021-02-04 22:20:42 -0300

    feat: add Validation layer

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Igor Custodio on 04/02/21.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
