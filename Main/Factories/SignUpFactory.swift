b6c3f79dcdc703d0108aab11581280fa027b40a2
Igor <igor.custodio8@gmail.com>
2021-02-06 15:32:31 -0300

    fix: add NSAllowsArbitraryLoads

--- Main/Factories/SignUpFactory.swift
+++ Main/Factories/SignUpFactory.swift
@@ -16,7 +16,7 @@ class SignUpFactory {
     static func makeController() -> SignUpViewController {
         let controller = SignUpViewController.instantiate()
         let emailValidator = EmailValidatorAdapter()
-        let url = URL(string: "")!
+        let url = URL(string: "http://localhost:5050/api/signup")!
         let alamofireAdapter = AlamofireAdapter()
         let remoteAddAccount = RemoteAddAcount(url: url, httpClient: alamofireAdapter)
         let presenter = SignUpPresenter(alertView: controller, emailValidator: emailValidator, addAccount: remoteAddAccount, loadingView: controller)
