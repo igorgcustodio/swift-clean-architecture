be63c9010d8effba9e14c6bdb138c74dfebd190c
Igor <igor.custodio8@gmail.com>
2021-02-06 17:36:31 -0300

    feat: threading validation

--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -19,6 +19,28 @@ class UseCaseFactory {
     }
     
     static func makeRemoteAddAccount() -> AddAccount {
-        return RemoteAddAcount(url: makeUrl(path: "signup"), httpClient: httpClient)
+        let remoteAddAccount = RemoteAddAcount(url: makeUrl(path: "signup"), httpClient: httpClient)
+        return MainQueueDispatchDecorator(remoteAddAccount)
+    }
+}
+
+public final class MainQueueDispatchDecorator<T> {
+    private let instance: T
+    
+    public init(_ instance: T) {
+        self.instance = instance
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else { return DispatchQueue.main.async(execute: completion) }
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: AddAccount where T: AddAccount {
+    public func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
+        instance.add(addAccountModel: addAccountModel) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
     }
 }
--- Presentation/Presenters/SignUp/SignUpPresenter.swift
+++ Presentation/Presenters/SignUp/SignUpPresenter.swift
@@ -26,16 +26,15 @@ public final class SignUpPresenter {
         if let message = validate(viewModel: viewModel) {
             self.alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
         } else {
-            loadingView.display(viewModel: LoadingViewModel(isLoading: true))
             addAccount.add(addAccountModel: SignUpMapper.toAddAccountModel(viewModel: viewModel)) { [weak self] result in
                 guard let self = self else { return }
+                self.loadingView.display(viewModel: LoadingViewModel(isLoading: true))
                 switch result {
                 case .failure:
                     self.alertView.showMessage(viewModel: AlertViewModel(title: "Erro", message: "Algo inesperado aconteceu, tente novamente em alguns instantes"))
                 case .success:
                     self.alertView.showMessage(viewModel: AlertViewModel(title: "Sucesso", message: "Conta criada com sucesso"))
                 }
-                self.loadingView.display(viewModel: LoadingViewModel(isLoading: false))
             }
         }
     }
