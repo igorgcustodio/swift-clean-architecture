ae18d50c3f8b778858c8e85aabeae6378ff136f3
Igor <igor.custodio8@gmail.com>
2021-04-01 19:59:16 -0300

    refactor: make RemoteAuth conforms to Authentication protocol

--- Data/UseCases/RemoteAuthentication.swift
+++ Data/UseCases/RemoteAuthentication.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Domain
 
-public final class RemoteAuthentication {
+public final class RemoteAuthentication: Authentication {
     
     private let url: URL
     private let httpClient: HttpPostClient
