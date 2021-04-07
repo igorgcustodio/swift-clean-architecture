9108d7eddf5627fef9c854db4d33691066f4b4ba
Igor <igor.custodio8@gmail.com>
2021-04-07 20:24:26 -0300

    refactor: change navigation init

--- Main/SceneDelegate.swift
+++ Main/SceneDelegate.swift
@@ -21,11 +21,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
+        let nav = NavigationController()
+        
         let httpClient = makeAlamofireAdapter()
         let addAccount = makeRemoteAddAccount(httpClient: httpClient)
         let signUpController = makeSignUpController(addAccount: addAccount)
         
-        let nav = NavigationController(rootViewController: signUpController)
+        nav.setRootViewController(signUpController)
         
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
--- UI/Components/NavigationController.swift
+++ UI/Components/NavigationController.swift
@@ -9,14 +9,18 @@ import UIKit
 
 public final class NavigationController: UINavigationController {
     
-    public override init(rootViewController: UIViewController) {
-        super.init(rootViewController: rootViewController)
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
         setup()
     }
     
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         setup()
+    }
+    
+    public convenience init() {
+        self.init(nibName: nil, bundle: nil)
     }
     
     private func setup() {
@@ -25,5 +29,13 @@ public final class NavigationController: UINavigationController {
         navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         navigationBar.isTranslucent = false
         navigationBar.barStyle = .black
+    }
+    
+    public func setRootViewController(_ viewController: UIViewController) {
+        setViewControllers([viewController], animated: true)
+    }
+    
+    public func pushViewController(_ viewController: UIViewController) {
+        pushViewController(viewController, animated: true)
     }
 }
